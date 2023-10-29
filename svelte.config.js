da5aa208588ab45d277412b24a35b1f8b62fa773
sakura-aoi-ororora <souyakoji@gmail.com>
2023-10-29 15:01:00 +0900

    CI修正

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.env.NODE_ENV
+const dev = process.argv.includes("dev")
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
