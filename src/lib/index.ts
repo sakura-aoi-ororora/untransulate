9413487bb1727f8b50d8e530c78f8748befef235
sakura-aoi-ororora <souyakoji@gmail.com>
2023-10-29 20:48:46 +0900

    レスポンス部分の作成

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,19 @@
+const translateLink = "https://script.google.com/macros/s/AKfycbwy0DX4Rlms3M_y3EOo5ZBtSDdUoOT2556ToxRwBYEkdlAV_aqALhUfFqCdrVJEgRBSFA/exec" + "?"
+
 // place files you want to import through the `$lib` alias in this folder.
+export async function transulateResponse(input: string, langList: string[]): Promise<string> {
+    const params = {
+        text: input,
+        languages: JSON.stringify(langList)
+    }
+    const urlParam = new URLSearchParams(params)
+    console.log(translateLink + urlParam)
+    const rest = await fetch(translateLink + urlParam, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    const data = (await rest.json()).message
+    console.log(data)
+    return data
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,17 @@
+<script lang="ts">
+	import { transulateResponse } from "$lib";
+
+    let input = ""
+    let output = ""
+    const mockLang: string[] = ["ja","eu","en","gn","ja"]
+    
+    async function onTransulate() {
+        transulateResponse(input, mockLang).then(res => output = res)
+    }
+</script>
+
 <h1>This page is Home!</h1>
+
+<textarea class="textarea textarea-bordered textarea-primary" bind:value={input} placeholder="source"/>
+<button class="btn btn-accent" on:click={onTransulate}>transulate</button>
+<textarea class="textarea textarea-bordered textarea-secondary" bind:value={output} placeholder="target"/>
