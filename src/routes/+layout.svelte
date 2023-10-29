a286f2238d81644b65b24b3c35a037c9ac843bd8
sakura-aoi-ororora <souyakoji@gmail.com>
2023-10-29 14:47:36 +0900

    create navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,19 @@
 <!-- Tailwindcss -->
-<script>
-    import "../app.css";
+<script lang="ts">
+	import { base } from '$app/paths';
+	import '../app.css';
 </script>
 
 <!-- navbar -->
-
+<div class="navbar glass bg-base-200">
+	<div class="navbar-start gap-1">
+		<a href="{base}/" class="btn btn-ghost text-xl normal-case"> UnTransulater </a>
+		<a href="{base}/lang" class="btn btn-primary text-xl normal-case"> Language </a>
+	</div>
+	<div class="navbar-end gap-1">
+		<a href="{base}/setting" class="btn btn-accent text-xl normal-case">Setting</a>
+		<a href="{base}/about" class="btn btn-info text-xl normal-case">About</a>
+	</div>
+</div>
 
 <slot />
---
+++ src/routes/lang/+page.svelte
@@ -0,0 +1,1 @@
+<h1>lang setting</h1>
